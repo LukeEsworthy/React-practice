9b4aa325b4a6b82fb774cc0494877f5f09b0dc04
Luke Esworthy <lukesworthy@gmail.com>
2020-05-28 14:23:39 -0500

    add/commit with completed chapter 6 before adding photos for fun

---
+++ src/components/location/LocationDetail.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import LocationManager from "../../modules/LocationManager";
+import "./LocationDetail.css";
+
+const LocationDetail = (props) => {
+  const [location, setLocation] = useState({ area: "", address: "" });
+
+  useEffect(() => {
+    //get(id) from LocationManager and hang on to the data; put it into state
+    LocationManager.get(props.locationId).then((location) => {
+      setLocation({
+        area: location.area,
+        address: location.address,
+      });
+    });
+  }, [props.locationId]);
+
+  return (
+    <div className="card">
+      <div className="card-content">
+        {/* <picture>
+          <img src={require("./dog.svg")} alt="My Dog" />
+        </picture> */}
+        <h3>
+          Name: <span style={{ color: "darkslategrey" }}>{location.area}</span>
+        </h3>
+        <p>Breed: {location.address}</p>
+      </div>
+    </div>
+  );
+};
+
+export default LocationDetail;
