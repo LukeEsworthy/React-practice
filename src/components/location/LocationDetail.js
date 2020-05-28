7271ab4999e85a7969e4375538beb1703ad2020d
Luke Esworthy <lukesworthy@gmail.com>
2020-05-28 15:29:58 -0500

    could not get photos to work, commit after reverting everything back to working code

--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -18,9 +18,6 @@ const LocationDetail = (props) => {
   return (
     <div className="card">
       <div className="card-content">
-        {/* <picture>
-          <img src={require("./dog.svg")} alt="My Dog" />
-        </picture> */}
         <h3>
           Name: <span style={{ color: "darkslategrey" }}>{location.area}</span>
         </h3>
