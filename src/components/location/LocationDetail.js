16e0793db6c5a60460030cce953c9a229160543c
Luke Esworthy <lukesworthy@gmail.com>
2020-05-29 13:09:47 -0500

    completion of chapter 7, adding delete

--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -4,6 +4,7 @@ import "./LocationDetail.css";
 
 const LocationDetail = (props) => {
   const [location, setLocation] = useState({ area: "", address: "" });
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     //get(id) from LocationManager and hang on to the data; put it into state
@@ -12,8 +13,17 @@ const LocationDetail = (props) => {
         area: location.area,
         address: location.address,
       });
+      setIsLoading(false);
     });
   }, [props.locationId]);
+
+  const handleDelete = () => {
+    //invoke the delete function in LocationDetail and re-direct to the location list.
+    setIsLoading(true);
+    LocationManager.delete(props.locationId).then(() =>
+      props.history.push("/locations")
+    );
+  };
 
   return (
     <div className="card">
@@ -22,6 +32,9 @@ const LocationDetail = (props) => {
           Name: <span style={{ color: "darkslategrey" }}>{location.area}</span>
         </h3>
         <p>Breed: {location.address}</p>
+        <button type="button" disabled={isLoading} onClick={handleDelete}>
+          Close Location
+        </button>
       </div>
     </div>
   );
